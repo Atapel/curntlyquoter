a286ae05b001034a89e0c13c1d51d41aac7dd491
Kilian Voss <kilian96@live.de>
2023-06-17 20:01:01 +1000

    Change in Datastructure

--- src/api_requests/fetch_products.jsx
+++ src/api_requests/fetch_products.jsx
@@ -1,39 +1,59 @@
 // can be fetched from the db in the future
 
-let products = [{
-        'Description':'UTS1200',
-        'Size': 9,
-        'SVG_str':'<svg width="178" height="46" viewBox="0 0 178 46" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M88.1504 19.4678L88.1725 26.4545M89.2393 26.4511L89.986 26.4487M90.9991 26.4455L90.977 19.4589M79.564 19.495L100.417 19.429L100.439 26.4157L79.5861 26.4816L79.564 19.495ZM1.13698 44.8632L177.136 44.3064L176.999 1L1 1.55671L1.13698 44.8632Z" stroke="black" stroke-width="1.28" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>'
-        
-    },
-    {
-        'Description':'UTS800',
-        'Size': 9,
-        'SVG_str':'<svg width="178" height="46" viewBox="0 0 178 46" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M88.1504 19.4678L88.1725 26.4545M89.2393 26.4511L89.986 26.4487M90.9991 26.4455L90.977 19.4589M79.564 19.495L100.417 19.429L100.439 26.4157L79.5861 26.4816L79.564 19.495ZM1.13698 44.8632L177.136 44.3064L176.999 1L1 1.55671L1.13698 44.8632Z" stroke="black" stroke-width="1.28" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>'
-    },
-    {
-        'Description':'UTS600',
-        'Size': 6,
-        'SVG_str':'<svg width="178" height="32" viewBox="0 0 178 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M88.1274 12.2145L88.1497 19.2545M89.2698 19.251L89.9631 19.2488M90.9763 19.2456L90.954 12.2056M79.541 12.2417L100.394 12.1757L100.417 19.2157L79.5633 19.2817L79.541 12.2417ZM1.09143 30.4632L177.091 29.9065L176.999 1L1 1.55671L1.09143 30.4632Z" stroke="black" stroke-width="1.28" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+let Single_breakers = [{
+            'Description':'UTS1200',
+            'Size': 9,
+            'SVG_str':'<svg width="178" height="46" viewBox="0 0 178 46" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M88.1504 19.4678L88.1725 26.4545M89.2393 26.4511L89.986 26.4487M90.9991 26.4455L90.977 19.4589M79.564 19.495L100.417 19.429L100.439 26.4157L79.5861 26.4816L79.564 19.495ZM1.13698 44.8632L177.136 44.3064L176.999 1L1 1.55671L1.13698 44.8632Z" stroke="black" stroke-width="1.28" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+            
+        },
+        {
+            'Description':'UTS800',
+            'Size': 9,
+            'SVG_str':'<svg width="178" height="46" viewBox="0 0 178 46" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M88.1504 19.4678L88.1725 26.4545M89.2393 26.4511L89.986 26.4487M90.9991 26.4455L90.977 19.4589M79.564 19.495L100.417 19.429L100.439 26.4157L79.5861 26.4816L79.564 19.495ZM1.13698 44.8632L177.136 44.3064L176.999 1L1 1.55671L1.13698 44.8632Z" stroke="black" stroke-width="1.28" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+        },
+        {
+            'Description':'UTS600',
+            'Size': 6,
+            'SVG_str':'<svg width="178" height="32" viewBox="0 0 178 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M88.1274 12.2145L88.1497 19.2545M89.2698 19.251L89.9631 19.2488M90.9763 19.2456L90.954 12.2056M79.541 12.2417L100.394 12.1757L100.417 19.2157L79.5633 19.2817L79.541 12.2417ZM1.09143 30.4632L177.091 29.9065L176.999 1L1 1.55671L1.09143 30.4632Z" stroke="black" stroke-width="1.28" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>'
 
-    },
-    {
-        'Description':'UTS400',
-        'Size': 6,
-        'SVG_str':'<svg width="178" height="32" viewBox="0 0 178 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M88.1274 12.2145L88.1497 19.2545M89.2698 19.251L89.9631 19.2488M90.9763 19.2456L90.954 12.2056M79.541 12.2417L100.394 12.1757L100.417 19.2157L79.5633 19.2817L79.541 12.2417ZM1.09143 30.4632L177.091 29.9065L176.999 1L1 1.55671L1.09143 30.4632Z" stroke="black" stroke-width="1.28" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>'
-    },
-    {
-        'Description':'UTS250',
-        'Size': 4,
-        'SVG_str':'<svg width="178" height="22" viewBox="0 0 178 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M88.1122 7.41443L88.1343 14.4011M89.2544 14.3975L89.9478 14.3953M90.961 14.3921L90.9389 7.40549M79.5258 7.44159L100.379 7.37563L100.401 14.3623L79.5479 14.4282L79.5258 7.44159ZM1.0609 20.8099L177.06 20.2532L176.999 1L1 1.55671L1.0609 20.8099Z" stroke="black" stroke-width="1.28" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>'
-        
-    },
-    {
-        'Description':'UTS150',
-        'Size': 4,
-        'SVG_str':'<svg width="178" height="22" viewBox="0 0 178 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M88.1122 7.41443L88.1343 14.4011M89.2544 14.3975L89.9478 14.3953M90.961 14.3921L90.9389 7.40549M79.5258 7.44159L100.379 7.37563L100.401 14.3623L79.5479 14.4282L79.5258 7.44159ZM1.0609 20.8099L177.06 20.2532L176.999 1L1 1.55671L1.0609 20.8099Z" stroke="black" stroke-width="1.28" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>'
-    }
-]
+        },
+        {
+            'Description':'UTS400',
+            'Size': 6,
+            'SVG_str':'<svg width="178" height="32" viewBox="0 0 178 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M88.1274 12.2145L88.1497 19.2545M89.2698 19.251L89.9631 19.2488M90.9763 19.2456L90.954 12.2056M79.541 12.2417L100.394 12.1757L100.417 19.2157L79.5633 19.2817L79.541 12.2417ZM1.09143 30.4632L177.091 29.9065L176.999 1L1 1.55671L1.09143 30.4632Z" stroke="black" stroke-width="1.28" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+        },
+        {
+            'Description':'UTS250',
+            'Size': 4,
+            'SVG_str':'<svg width="178" height="22" viewBox="0 0 178 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M88.1122 7.41443L88.1343 14.4011M89.2544 14.3975L89.9478 14.3953M90.961 14.3921L90.9389 7.40549M79.5258 7.44159L100.379 7.37563L100.401 14.3623L79.5479 14.4282L79.5258 7.44159ZM1.0609 20.8099L177.06 20.2532L176.999 1L1 1.55671L1.0609 20.8099Z" stroke="black" stroke-width="1.28" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+            
+        },
+        {
+            'Description':'UTS150',
+            'Size': 4,
+            'SVG_str':'<svg width="178" height="22" viewBox="0 0 178 22" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M88.1122 7.41443L88.1343 14.4011M89.2544 14.3975L89.9478 14.3953M90.961 14.3921L90.9389 7.40549M79.5258 7.44159L100.379 7.37563L100.401 14.3623L79.5479 14.4282L79.5258 7.44159ZM1.0609 20.8099L177.06 20.2532L176.999 1L1 1.55671L1.0609 20.8099Z" stroke="black" stroke-width="1.28" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+        }
+    ]
+
+    let Double_breakers = [{
+
+            'Description':'UTS400',
+            'Size': 6,
+            'SVG_str':'<svg width="178" height="32" viewBox="0 0 178 32" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M46.4424 19.6673L46.4189 12.6807M45.352 12.6842L44.6053 12.6868M43.5923 12.6902L43.6158 19.6768M134.176 19.585L134.153 12.5983M133.032 12.6021L132.339 12.6045M131.326 12.6079L131.349 19.5945M55.0825 19.6382L34.176 19.7086L34.1524 12.722L55.059 12.6515L55.0825 19.6382ZM142.816 19.5558L121.91 19.6263L121.886 12.6397L142.793 12.5692L142.816 19.5558ZM177.048 1.30704L1.04881 1.90022L1.14605 30.7534L177.145 30.1602L177.048 1.30704Z" stroke="black" stroke-width="1.28" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+        },
+        {
+            'Description':'UTS250',
+            'Size': 4,
+            'SVG_str':'<svg width="178" height="23" viewBox="0 0 178 23" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M46.3706 15.5345L46.313 8.54805M45.2461 8.55684L44.4995 8.563M43.4864 8.57135L43.544 15.5578M134.103 15.0247L134.045 8.03825M132.925 8.04748L132.232 8.05319M131.219 8.06154L131.276 15.048M55.0104 15.4633L34.1045 15.6356L34.0469 8.64914L54.9528 8.47684L55.0104 15.4633ZM142.743 14.9535L121.837 15.1258L121.779 8.13934L142.685 7.96704L142.743 14.9535ZM176.925 1.33795L0.930499 2.78845L1.08917 22.0411L177.083 20.5906L176.925 1.33795Z" stroke="black" stroke-width="1.28" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+            
+        },
+        {
+            'Description':'UTS150',
+            'Size': 4,
+            'SVG_str':'<svg width="178" height="23" viewBox="0 0 178 23" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M46.3706 15.5345L46.313 8.54805M45.2461 8.55684L44.4995 8.563M43.4864 8.57135L43.544 15.5578M134.103 15.0247L134.045 8.03825M132.925 8.04748L132.232 8.05319M131.219 8.06154L131.276 15.048M55.0104 15.4633L34.1045 15.6356L34.0469 8.64914L54.9528 8.47684L55.0104 15.4633ZM142.743 14.9535L121.837 15.1258L121.779 8.13934L142.685 7.96704L142.743 14.9535ZM176.925 1.33795L0.930499 2.78845L1.08917 22.0411L177.083 20.5906L176.925 1.33795Z" stroke="black" stroke-width="1.28" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/></svg>'
+        }
+    ]
 
 
-export default products
+
+export {Single_breakers, Double_breakers}
