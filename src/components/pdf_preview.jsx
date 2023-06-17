2cec3f5e3d3dafbca61e0cb7ab64358153d0235d
Kilian Voss <kilian96@live.de>
2023-06-17 20:01:58 +1000

    Breakers are now getting rendered inside the frame

--- src/components/pdf_preview.jsx
+++ src/components/pdf_preview.jsx
@@ -6,62 +6,56 @@ import DisplaySelectedItems from './configuration_preview.jsx'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
+import { containe_w_dimensions_SVG } from './switch_board.jsx';
 const PDF_preview = () => {
     const { Selected_Items, set_Selected_Items } = useContext(Selected_Items_Context);
     const canvasRef = useRef(null);
 
+    // Define your containerSVG string
+    const containerSVG =containe_w_dimensions_SVG
+    
     useEffect(() => {
         const canvas = canvasRef.current;
         const context = canvas.getContext('2d');
+    
+        // Load and draw the container SVG first
+        const container = new Image();
+        container.onload = () => {
+            // Set the canvas size to match the container image size
+            canvas.width = container.width;
+            canvas.height = container.height;
 
-        let currentY = 0;  // Initial y-coordinate
-
-        Selected_Items.forEach(item => {
-            const img = new Image();
-            img.onload = () => {
-                context.drawImage(img, 0, currentY);
-                if(item.Size == 9){
-                    currentY += 50;  // Increment y-coordinate
-                }else if(item.Size == 6){
-                    currentY += 35;  // Increment y-coordinate
-                }else if(item.Size == 4){
-                    currentY += 25;  // Increment y-coordinate
+            context.drawImage(container, 0, 0);
+    
+            // Once the container is drawn, proceed with the SVG items
+            let currentY = 117;  // Start with an offset for y-coordinate
+            let currentX = 78.5;  // Start with an offset for x-coordinate
+    
+            Selected_Items.forEach(item => {
+                const img = new Image();
+                img.onload = () => {
+                    context.drawImage(img, currentX, currentY);
+                    if(item.Size == 9){
+                        currentY += 45;  // Increment y-coordinate
+                    }else if(item.Size == 6){
+                        currentY += 30;  // Increment y-coordinate
+                    }else if(item.Size == 4){
+                        currentY += 20;  // Increment y-coordinate
+                    }
                 }
-                
-            }
-            img.src = 'data:image/svg+xml,' + encodeURIComponent(item.SVG_str);
-        });
-
-    }, [Selected_Items]);  // Dependency array
-
-    const canvasHeight = 50 * Selected_Items.length;  // Calculate the needed canvas height
+                img.src = 'data:image/svg+xml,' + encodeURIComponent(item.SVG_str);
+            });
+        };
+        container.src = 'data:image/svg+xml,' + encodeURIComponent(containerSVG);
+    
+    }, [Selected_Items, containerSVG]);  // Dependency array
+    
 
     const createPdf = () => {
         const pdf = new jsPDF();
         pdf.addImage(canvasRef.current.toDataURL('image/png'), 'PNG', 0, 0);
         pdf.save('Curntly_Config.pdf');
     };
-
-    // const createPdf = () => {
-    //     const pdf = new jsPDF('p', 'mm', 'a4'); // 'p' for portrait mode, 'mm' for unit in millimeters, and 'a4' for page size
-    //     const imgData = canvasRef.current.toDataURL('image/png');
-    //     const pdfWidth = pdf.internal.pageSize.getWidth();
-    //     const pdfHeight = pdf.internal.pageSize.getHeight();
-    
-    //     // Create a new image object to get the actual dimensions of the image
-    //     const img = new Image();
-    //     img.onload = function() {
-    //         const imgWidth = this.width;
-    //         const imgHeight = this.height;
-    //         const marginLeft = (pdfWidth - imgWidth / 4) / 2; // Calculate horizontal margin
-    //         const marginTop = (pdfHeight - imgHeight / 4) / 2; // Calculate vertical margin
-    
-    //         pdf.addImage(imgData, 'PNG', marginLeft, marginTop, imgWidth / 4, imgHeight / 4); // Add the image with the correct dimensions and margins
-    //         pdf.save('Curntly_Config.pdf');
-    //     };
-    //     img.src = imgData;
-    // };
 
     return (
         <div>
@@ -71,7 +65,7 @@ const PDF_preview = () => {
                         <DisplaySelectedItems />
                     </Col>
                     <Col>
-                        <canvas ref={canvasRef} height={canvasHeight} />
+                        <canvas ref={canvasRef} />
                         {Selected_Items && Selected_Items.length > 0 ? (
                             <Button variant="success" onClick={createPdf}>Download PDF</Button>
                             ) : (
@@ -80,12 +74,13 @@ const PDF_preview = () => {
                     </Col>
                 </Row>
             </Container>
-
         </div>
     );
 };
 
 export default PDF_preview;
+
+
 
 
 
