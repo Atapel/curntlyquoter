213c9af54daac532f622731ed8346036309fe7ee
Kilian Voss <kilian96@live.de>
2023-06-20 00:20:50 +1000

    new context

--- src/selected_items_context.jsx
+++ src/selected_items_context.jsx
@@ -2,3 +2,4 @@ import React from "react";
 
 export const Selected_Items_Context = React.createContext()
 export const Items_Lenght_Check_Context = React.createContext()
+export const User_Input_Context = React.createContext()
