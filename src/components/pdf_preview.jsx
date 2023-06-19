9040164e7ad7b2b077cc37cdf01492e0c37009ea
Kilian Voss <kilian96@live.de>
2023-06-20 00:19:41 +1000

    import changes, progress in creating and rendering the pdf const createPdf = () => {...}

--- src/components/pdf_preview.jsx
+++ src/components/pdf_preview.jsx
@@ -1,18 +1,22 @@
 import React, { useContext, useEffect, useRef } from 'react';
-import { Selected_Items_Context } from '../selected_items_context.jsx';
+import { Selected_Items_Context, User_Input_Context } from '../selected_items_context.jsx';
 import { jsPDF } from "jspdf";
 import Button from 'react-bootstrap/Button';
 import DisplaySelectedItems from './configuration_preview.jsx'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import { containe_w_dimensions_SVG } from '../assets/switch_board.jsx';
+import { container_46_w_dimensions_SVG } from '../assets/switch_board.jsx';
+// import PDF_generator from './pdf_generator.jsx';
+
 const PDF_preview = () => {
     const { Selected_Items, set_Selected_Items } = useContext(Selected_Items_Context);
+    const { User_Input, setUser_Input } = useContext(User_Input_Context);
+
     const canvasRef = useRef(null);
 
     // Define your containerSVG string
-    const containerSVG =containe_w_dimensions_SVG
+    const containerSVG = container_46_w_dimensions_SVG
     
     useEffect(() => {
         const canvas = canvasRef.current;
@@ -51,11 +55,45 @@ const PDF_preview = () => {
     }, [Selected_Items, containerSVG]);  // Dependency array
     
 
+    
+
     const createPdf = () => {
-        const pdf = new jsPDF();
-        pdf.addImage(canvasRef.current.toDataURL('image/png'), 'PNG', 0, 0);
+        const pdf = new jsPDF({
+            orientation: "landscape",
+            unit: "mm",
+            format: "a4"
+        });
+
+        pdf.addImage(canvasRef.current.toDataURL('image/png'), 'PNG', 5, 5);
+        pdf.setFontSize(10)
+        // Set the text color, draw color, and font:
+        pdf.setTextColor(0, 0, 0); // RGB values
+        pdf.setDrawColor(255, 0, 0); // RGB values
+        pdf.setFont("Helvetica", "normal"); // font name, style
+        // Add some text to the document at a specific position:
+        // Define your text lines
+        var lines = [
+            `Project: ${User_Input.project}`,
+            `Client: ${User_Input.client}`,
+            `Equipment: ${User_Input.equipment}`,
+            `Sales Order Number: ${User_Input.salesOrderNumber}`,
+            `Revision: ${User_Input.revision}`,
+            `Drawing Date: ${User_Input.drawingDate}`,
+            `Drawn By: ${User_Input.drawnBy}`,
+        ];
+        
+        // Start y-coordinate
+        var y = 170;
+        
+        // Print each line separately
+        for (var i = 0; i < lines.length; i++) {
+            pdf.text(lines[i], 240, y);
+            y += 5; // Increment y-coordinate for the new line
+        }
+    
         pdf.save('Curntly_Config.pdf');
-    };
+
+    }
 
     return (
         <div>
@@ -66,11 +104,16 @@ const PDF_preview = () => {
                     </Col>
                     <Col>
                         <canvas ref={canvasRef} />
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
                         {Selected_Items && Selected_Items.length > 0 ? (
                             <Button variant="success" onClick={createPdf}>Download PDF</Button>
                             ) : (
-                                <div />
+                             <div />
                         )}
+                        
                     </Col>
                 </Row>
             </Container>
@@ -79,10 +122,3 @@ const PDF_preview = () => {
 };
 
 export default PDF_preview;
-
-
-
-
-
-
-
