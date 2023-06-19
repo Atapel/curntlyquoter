d442492b691cb3ced57f176df927cabe6863ff0c
Kilian Voss <kilian96@live.de>
2023-06-20 00:22:30 +1000

    implemented the user_input config with corresponding State and context variables into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,36 @@ import { useState, useRef } from 'react';
 import './App.css';
 import PDF_preview from './components/pdf_preview'
 import Select_Breakers_Menu from './components/dropdown_product_selector';
+import New_config_input from './components/user_input';
 
-import { Selected_Items_Context, Items_Lenght_Check_Context } from './selected_items_context.jsx';
+import { Selected_Items_Context, Items_Lenght_Check_Context, User_Input_Context } from './selected_items_context.jsx';
 function App() {
   const [Selected_Items, set_Selected_Items] = useState([]);
   const [Length_Limit_Check, setLength_Limit_Check] = useState(0);
+  const [User_Input, setUser_Input] = useState({
+    client: '',
+    project: '',
+    equipment: '-',
+    salesOrderNumber: '-',
+    revision: '-',
+    drawingDate: '',
+    drawnBy: ''
+  })
 
   return (
     <Selected_Items_Context.Provider value={{Selected_Items, set_Selected_Items}}>
       <Items_Lenght_Check_Context.Provider value={{Length_Limit_Check, setLength_Limit_Check}}>
+        <User_Input_Context.Provider value={{User_Input, setUser_Input}}>
     <>
       {/* Header */}
       <h1>Configurator Prototype</h1>
       
       {/* Main Section */}
       <div>
+        {/* Create new configuration/project */}
+        <div>
+          <New_config_input />
+        </div>
         {/* Breaker selection menu */}
         <div>
           <Select_Breakers_Menu />
@@ -30,6 +45,7 @@ function App() {
       </div>
       
     </>
+    </User_Input_Context.Provider>
     </Items_Lenght_Check_Context.Provider>
     </Selected_Items_Context.Provider>
   )
