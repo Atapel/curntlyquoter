36ce6a6fe40b734a10a39a2ea481d10d54bd4dc4
Kilian Voss <kilian96@live.de>
2023-06-20 21:31:59 +1000

    styling changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState, useRef } from 'react';
-
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 import './App.css';
+import DisplaySelectedItems from './components/configuration_preview'
 import PDF_preview from './components/pdf_preview'
 import Select_Breakers_Menu from './components/dropdown_product_selector';
 import New_config_input from './components/user_input';
-
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { Selected_Items_Context, Items_Lenght_Check_Context, User_Input_Context } from './selected_items_context.jsx';
 function App() {
   const [Selected_Items, set_Selected_Items] = useState([]);
@@ -23,30 +27,38 @@ function App() {
     <Selected_Items_Context.Provider value={{Selected_Items, set_Selected_Items}}>
       <Items_Lenght_Check_Context.Provider value={{Length_Limit_Check, setLength_Limit_Check}}>
         <User_Input_Context.Provider value={{User_Input, setUser_Input}}>
-    <>
-      {/* Header */}
-      <h1>Configurator Prototype</h1>
-      
-      {/* Main Section */}
-      <div>
-        {/* Create new configuration/project */}
-        <div>
-          <New_config_input />
-        </div>
-        {/* Breaker selection menu */}
-        <div>
-          <Select_Breakers_Menu />
-        </div>
+          <>
 
-        {/* Breaker selection Preview */}
-        <div>
-          <PDF_preview />
-        </div>
-      </div>
-      
-    </>
-    </User_Input_Context.Provider>
-    </Items_Lenght_Check_Context.Provider>
+            {/* Header */}
+            <Tabs
+              defaultActiveKey="Start"
+              fill
+            >
+
+              <Tab eventKey="Start" title="Start new Drawing">
+                <New_config_input />
+              </Tab>
+              
+              <Tab eventKey="Configure" title="Configurator">
+                <Row>
+                    <Col md={4}>
+                        <Select_Breakers_Menu />
+                        <DisplaySelectedItems />
+                    </Col>
+                    <Col md={8}>
+                        <PDF_preview />
+                    </Col>
+                </Row>            
+              </Tab>
+
+              <Tab eventKey="Quote" title="Get Quote">
+                
+              </Tab>
+                
+            </Tabs>
+          </>
+        </User_Input_Context.Provider>
+      </Items_Lenght_Check_Context.Provider>
     </Selected_Items_Context.Provider>
   )
 }
