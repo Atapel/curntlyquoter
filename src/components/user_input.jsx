430dd6cfa463282d1aae09262d40d3a3413ab00a
Kilian Voss <kilian96@live.de>
2023-07-02 15:00:47 +1000

    linting

--- src/components/user_input.jsx
+++ src/components/user_input.jsx
@@ -1,31 +1,34 @@
-import React, { useState, useContext } from 'react';
-import { Form, Button, Alert } from 'react-bootstrap';
-import { Row, Col, Container } from 'react-bootstrap';
-import { User_Input_Context } from '../selected_items_context.jsx';
+import React, { useState, useContext } from "react";
+import { Form, Button } from "react-bootstrap";
+import { Row, Col, Container } from "react-bootstrap";
+import { User_Input_Context } from "../selected_items_context.jsx";
 
 const New_config_input = () => {
-  const {User_Input, setUser_Input} = useContext(User_Input_Context);
+  const { User_Input, setUser_Input } = useContext(User_Input_Context);
+  const [showAlert, setShowAlert] = useState(false);
 
   const handleInputChange = (event) => {
     setUser_Input({
       ...User_Input,
-      [event.target.name]: event.target.value
+      [event.target.name]: event.target.value,
     });
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (Object.values(User_Input).some(value => value === '')) {
-      alert("Please fill out all fields before submitting.");
+    if (Object.values(User_Input).some((value) => value === "")) {
+      setShowAlert(true);
     } else {
+      setShowAlert(false);
       console.log(User_Input);
     }
   };
 
-  const isFormIncomplete = Object.values(User_Input).some(value => value === '');
+  const isFormIncomplete = Object.values(User_Input).some(
+    (value) => value === ""
+  );
 
   return (
- 
     <Form onSubmit={handleSubmit}>
       <Container>
         <Form.Group controlId="formClient">
@@ -34,12 +37,16 @@ const New_config_input = () => {
               <Form.Label>Client</Form.Label>
             </Col>
             <Col>
-              <Form.Control type="text" name="client" onChange={handleInputChange} />
+              <Form.Control
+                type="text"
+                name="client"
+                onChange={handleInputChange}
+              />
             </Col>
           </Row>
         </Form.Group>
       </Container>
-      
+
       <Container>
         <Form.Group controlId="formProject">
           <Row>
@@ -47,7 +54,11 @@ const New_config_input = () => {
               <Form.Label>Project</Form.Label>
             </Col>
             <Col>
-              <Form.Control type="text" name="project" onChange={handleInputChange} />
+              <Form.Control
+                type="text"
+                name="project"
+                onChange={handleInputChange}
+              />
             </Col>
           </Row>
         </Form.Group>
@@ -60,7 +71,11 @@ const New_config_input = () => {
               <Form.Label>Drawing Date</Form.Label>
             </Col>
             <Col>
-              <Form.Control type="date" name="drawingDate" onChange={handleInputChange} />
+              <Form.Control
+                type="date"
+                name="drawingDate"
+                onChange={handleInputChange}
+              />
             </Col>
           </Row>
         </Form.Group>
@@ -68,28 +83,25 @@ const New_config_input = () => {
 
       <Container>
         <Form.Group controlId="formDrawnBy">
-        <Row>
-          <Col>
-            <Form.Label>Drawn By</Form.Label>
-          </Col>
-          <Col>
-            <Form.Control type="text" name="drawnBy" onChange={handleInputChange} />
-          </Col>
-        </Row>
+          <Row>
+            <Col>
+              <Form.Label>Drawn By</Form.Label>
+            </Col>
+            <Col>
+              <Form.Control
+                type="text"
+                name="drawnBy"
+                onChange={handleInputChange}
+              />
+            </Col>
+          </Row>
         </Form.Group>
       </Container>
-      {isFormIncomplete && <Alert variant="warning">Please fill out all fields before submitting.</Alert>}
-
       <Button variant="primary" type="submit" disabled={isFormIncomplete}>
         Submit
       </Button>
-      
     </Form>
   );
 };
 
 export default New_config_input;
-
-
-
-
