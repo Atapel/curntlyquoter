57d2af4dd50704c358bcd7af8588e5890db02de8
Kilian Voss <kilian96@live.de>
2023-07-12 22:15:40 +1000

    component connects to db and loads previously saved configurations

---
+++ src/components/Admin_page_saved_configurations.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from "react";
+import { supabase } from "../supabaseClient";
+import {
+  Dropdown,
+  Button,
+  Row,
+  Col,
+  ListGroup,
+  ListGroupItem,
+  Card,
+} from "react-bootstrap";
+
+function Saved_Configurations() {
+  const [configs, setConfigs] = useState([]);
+
+  async function getConfigs() {
+    try {
+      const { data, error } = await supabase.from("Configurations").select();
+      if (error) {
+        throw new Error("Failed to retrieve data from the database.");
+      }
+      setConfigs(data);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getConfigs();
+  }, []);
+
+  return (
+    <>
+      <ListGroup>
+        <ListGroup.Item>
+          <h2>Previous configurations</h2>
+        </ListGroup.Item>
+        <ListGroup.Item>
+          <Row>
+            {configs.map((configuration) => (
+              <>
+                <Card style={{ width: "18rem" }} key={configuration.id}>
+                  <Card.Img variant="top" src="" />
+                  <Card.Body>
+                    <Card.Title>{configuration.init_project}</Card.Title>
+                    <ListGroup>
+                      <ListGroupItem>{configuration.created_at}</ListGroupItem>
+                      <ListGroupItem>{configuration.init_client}</ListGroupItem>
+                    </ListGroup>
+                    <Button variant="primary">Expand... </Button>
+                    {/* <Button variant="primary">Load Configuration</Button> */}
+                  </Card.Body>
+                </Card>
+              </>
+            ))}
+          </Row>
+        </ListGroup.Item>
+      </ListGroup>
+    </>
+  );
+}
+
+export default Saved_Configurations;
