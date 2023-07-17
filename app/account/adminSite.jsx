3d34df1dfe413d7da4fdcbfe223e0af40e38186d
Kilian Voss <kilian96@live.de>
2023-07-17 15:27:31 +1000

    integrating all admin sites into a ssr component

--- app/account/adminSite.jsx
+++ app/account/adminSite.jsx
@@ -1,13 +1,13 @@
 import React from "react"
 import UserPanel from "./components/adminUserPanel";
-// import NewConfigInput from "./components/adminUserInput"
+import NewConfigInput from "./components/adminUserInput"
 
 export default async function AdminSite({session} ) {
 
   return (
     <>
       <UserPanel session={session} />
-      {/* <NewConfigInput /> */}
+      <NewConfigInput />
     </>
   );
 }
