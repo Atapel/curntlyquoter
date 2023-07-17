9c848bcdb139cf121e71dc70eb7005f8701efef2
Kilian Voss <kilian96@live.de>
2023-07-17 23:26:05 +1000

    integrated/activated new config structure

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-// import { GlobalContextProvider } from "./context/globalContext";
+import { GlobalContextProvider } from "./context/globalContext";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,9 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      {/* <GlobalContextProvider> */}
+      <GlobalContextProvider>
       <body className={inter.className}>{children}</body>
-      {/* </GlobalContextProvider> */}
+      </GlobalContextProvider>
     </html>
   );
 }
