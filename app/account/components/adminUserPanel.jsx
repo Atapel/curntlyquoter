c3452de2809d3fabb81cd537ef92bad144be3ef3
Kilian Voss <kilian96@live.de>
2023-07-19 11:29:29 +1000

    rendering logic to indicate if user is signed in or not

--- app/account/components/adminUserPanel.jsx
+++ app/account/components/adminUserPanel.jsx
@@ -7,28 +7,55 @@ import { UseCurrentUserContext } from "@/app/context/globalContext";
 const UserPanel = ({ session }) => {
   const supabase = createClientComponentClient();
   const { CurrentUser, setCurrentUser } = UseCurrentUserContext();
-  setCurrentUser(session?.user);
 
   const handleSignOut = async () => {
     const { error } = await supabase.auth.signOut();
   };
 
+  if (session) {
+    setCurrentUser(session?.user);
+  }
+
   return (
-    <div className="form-widget">
-      <Row>
-        <Col>{session?.user.email}</Col>
-        <form action="/auth/signout" method="post">
-          <Button
-            variant="danger"
-            className="w-50"
-            onClick={handleSignOut}
-            type="submit"
-          >
-            Sign out
-          </Button>
-        </form>
-      </Row>
-    </div>
+    <ListGroup>
+      <ListGroup.Item>
+        <h2>Account</h2>
+      </ListGroup.Item>
+      <ListGroup.Item>
+        <Row>
+          {session ? (
+            <>
+              <Col>User: {session?.user.email}</Col>
+              <Col>
+                <form action="/auth/signout" method="post">
+                  <Button
+                    variant="danger"
+                    className="w-50"
+                    onClick={handleSignOut}
+                    type="submit"
+                  >
+                    Sign out
+                  </Button>
+                </form>
+              </Col>
+            </>
+          ) : (
+            <>
+              <Col>No user is signed in.</Col>
+              <Col>
+                <Button
+                  variant="success"
+                  href="/"
+                  className="w-50"
+                >
+                  Sign in here
+                </Button>
+              </Col>
+            </>
+          )}
+        </Row>
+      </ListGroup.Item>
+    </ListGroup>
   );
 };
 
