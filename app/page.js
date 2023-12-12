876161b569462d830bcf50a5d8848d0b8d088d0a
Kilian Voss <kilianvoss@proton.me>
2023-12-12 14:26:12 +1100

    ad conditional rendering logic, could be faulty

--- app/page.js
+++ app/page.js
@@ -1,7 +1,20 @@
-"use client";
-import AuthForm from "./auth-form";
+import Authenticate from "./auth/page"
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
-export default function Home() {
-  return <AuthForm />;
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies });
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    redirect('/auth');
+  }  else {
+    redirect('/account');
+  }
+  return <Authenticate />;
 }
 
