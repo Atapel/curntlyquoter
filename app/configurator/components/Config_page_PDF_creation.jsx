3db5c77701464cb352f289ec2d02d52f807956a8
Kilian Voss <kilianvoss@proton.me>
2023-12-15 13:19:06 +1100

    Printing Curntly Logo onto PDF, not using user selected date on the PDF anymore, using Name provided on signup on PDF, adding logic to retrieve userdetails

--- app/configurator/components/Config_page_PDF_creation.jsx
+++ app/configurator/components/Config_page_PDF_creation.jsx
@@ -1,28 +1,54 @@
+"use client"
 import React, { useEffect, useRef, useState } from "react";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { UseCurrentUserContext, UseUserInputContext } from "../../context/globalContext";
+import { UseUserInputContext } from "../../context/globalContext";
+import Curntly_Logo from "public/curntly_slogan.png";
+
 import { jsPDF } from "jspdf";
 import { Alert, Button } from "react-bootstrap";
 
 const PDF_Generation = ({ canvasRef }) => {
   const supabase = createClientComponentClient();
   const { User_Input, setUser_Input } = UseUserInputContext();
-  const { CurrentUser, setCurrentUser } = UseCurrentUserContext();
+  const [ProjectMetadata, setProjectMetadata] = useState(null)
   const [feedback, setFeedback] = useState(null);
   const [feedbackType, setFeedbackType] = useState(null);
 
-  async function uploadFile(file) {
-    const { data, error } = await supabase.storage
-      .from("Configuration_Drawings")
-      .upload(`Currntly_${CurrentUser.id}`, file);
-    if (error) {
-      setFeedbackType("danger");
-      setFeedback(`Upload failed: ${error.message}`);
-    } else {
-      setFeedbackType("success");
-      setFeedback("File uploaded successfully!");
+  const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
+  const americanFormatDate = new Date().toLocaleDateString('en-US', options);
+
+
+  useEffect(() => {
+    async function fetchData() {
+      const supabase = createClientComponentClient();
+
+      try {
+        const { data: { user } } = await supabase.auth.getUser();
+        console.log(user);
+        const { data, error } = await supabase
+          .from("User_Metadata")
+          .select('*')
+          .eq('User_UID', user.id); // Replace 'User_UID' with the actual column name
+
+        console.log(data);
+
+        setProjectMetadata(data[0])
+
+        if (error) {
+          console.error("Supabase error:", error.message, error.details);
+          throw new Error("Failed to insert record into the database.");
+          // You can handle success here or set a state to update the component
+        }
+        // Continue with handling data or setting state for success
+      } catch (error) {
+        console.error("Catch block error:", error);
+        // You can handle the error here or set a state to update the component
+      }
     }
-  }
+
+    fetchData();
+  }, []);
+
 
   const createPdf = () => {
     const pdf = new jsPDF({
@@ -38,16 +64,15 @@ const PDF_Generation = ({ canvasRef }) => {
     pdf.setDrawColor(255, 0, 0); // RGB values
     pdf.setFont("Helvetica", "normal"); // font name, style
     // Add some text to the document at a specific position:
-    // Define your text lines
-    
+
     var lines = [
       `Project: ${User_Input.project}`,
-      `Client: ${User_Input.client}`,
-      `Equipment: ${User_Input.equipment}`,
-      `Sales Order Number: ${User_Input.salesOrderNumber}`,
-      `Revision: ${User_Input.revision}`,
-      `Drawing Date: ${User_Input.drawingDate}`,
-      `Drawn By: ${User_Input.drawnBy}`,
+      `Client: ${User_Input.project}`,
+      `Equipment: ${0}`,
+      `Sales Order Number: ${0}`,
+      `Revision: ${0}`,
+      `Drawing Date: ${americanFormatDate}`,
+      `Drawn By: ${ProjectMetadata.Given_Name + " " + ProjectMetadata.Family_Name}`,
     ];
 
     // Start y-coordinate
@@ -58,6 +83,8 @@ const PDF_Generation = ({ canvasRef }) => {
       pdf.text(lines[i], 240, y);
       y += 5; // Increment y-coordinate for the new line
     }
+
+    pdf.addImage(Curntly_Logo.src, 'PNG', 239, 130, 50, 25);
 
     pdf.save("Curntly_Config.pdf");
 
