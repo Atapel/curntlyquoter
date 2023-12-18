270f0f57ff15fbc716fc3f66602d7568b8a19c35
Kilian Voss <kilianvoss@proton.me>
2023-12-18 19:44:11 +1100

    added Reset Password Button to Auth PAge

--- app/auth/page.jsx
+++ app/auth/page.jsx
@@ -3,6 +3,7 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { cookies } from 'next/headers';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
+
 export default async function Authenticate() {
     const supabase = createServerComponentClient({ cookies });
     const {
@@ -15,15 +16,16 @@ export default async function Authenticate() {
 
     return (
         <div className="container mt-5">
-            <div className="row">
-                <div className="col-md-6">
-                    <Link href="/auth/SignIn" className="btn btn-primary btn-lg btn-block">
+            <div className="row justify-content-center align-items-center">
+                <div className="col-md-4">
+                    <Link href="/auth/SignIn" className="btn btn-primary btn-lg btn-block mb-3">
                         Sign in if you already have an account
                     </Link>
-                </div>
-                <div className="col-md-6">
-                    <Link href="/auth/SignUp" className="btn btn-success btn-lg btn-block">
+                    <Link href="/auth/SignUp" className="btn btn-success btn-lg btn-block mb-3">
                         Sign up if you're new here
+                    </Link>
+                    <Link href="/auth/ResetPassword" className="btn btn-warning btn-lg btn-block mb-3">
+                        Reset your Password
                     </Link>
                 </div>
             </div>
