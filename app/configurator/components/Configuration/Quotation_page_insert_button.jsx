7522e20917dccfaf898ab1facfffb10661415207
Kilian Voss <kilianvoss@proton.me>
2023-12-22 15:01:29 +1100

    log statement outside of try catch block

--- app/configurator/components/Configuration/Quotation_page_insert_button.jsx
+++ app/configurator/components/Configuration/Quotation_page_insert_button.jsx
@@ -14,19 +14,20 @@ function SaveConfigurationButton({
   const currentTime = new Date().toISOString().substring(0, 19).replace("T", " ");
 
   async function insertConfigurations() {
-    try {
-      console.log({created_at: currentTime,
-        user_id: CurrentUser.User_UID,
-        init_client: User_Input.client,
-        init_project: User_Input.project,
-        init_drawn_by: CurrentUser.Given_Name + " " + CurrentUser.Family_Name,
+    console.log({created_at: currentTime,
+      user_id: CurrentUser.User_UID,
+      init_client: User_Input.client,
+      init_project: User_Input.project,
+      init_drawn_by: CurrentUser.Given_Name + " " + CurrentUser.Family_Name,
 
-        panel_width: Selected_Panel.Frame_Size,
-        panel_voltage: Selected_Panel.Voltage,
-        panel_KAIC_rating: Selected_Panel.KAIC_rating,
-        panel_bus_rating: Selected_Panel.Bus_rating,
-        selected_breakers: Selected_Breakers,
-        order_confirmed: false,});
+      panel_width: Selected_Panel.Frame_Size,
+      panel_voltage: Selected_Panel.Voltage,
+      panel_KAIC_rating: Selected_Panel.KAIC_rating,
+      panel_bus_rating: Selected_Panel.Bus_rating,
+      selected_breakers: Selected_Breakers,
+      order_confirmed: false,});
+    try {
+      
       // Try to insert data into the "Configurations" table
       const { error: insertError } = await supabase.from("Configurations").insert({
         created_at: currentTime,
