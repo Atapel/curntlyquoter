4578b2d44a43078cc24f6c0b05a035f0a885ad42
Kilian Voss <kilianvoss@proton.me>
2023-12-22 14:58:46 +1100

    logging the entire configuration object before inserting it

--- app/configurator/components/Configuration/Quotation_page_insert_button.jsx
+++ app/configurator/components/Configuration/Quotation_page_insert_button.jsx
@@ -15,6 +15,18 @@ function SaveConfigurationButton({
 
   async function insertConfigurations() {
     try {
+      console.log({created_at: currentTime,
+        user_id: CurrentUser.User_UID,
+        init_client: User_Input.client,
+        init_project: User_Input.project,
+        init_drawn_by: CurrentUser.Given_Name + " " + CurrentUser.Family_Name,
+
+        panel_width: Selected_Panel.Frame_Size,
+        panel_voltage: Selected_Panel.Voltage,
+        panel_KAIC_rating: Selected_Panel.KAIC_rating,
+        panel_bus_rating: Selected_Panel.Bus_rating,
+        selected_breakers: Selected_Breakers,
+        order_confirmed: false,});
       // Try to insert data into the "Configurations" table
       const { error: insertError } = await supabase.from("Configurations").insert({
         created_at: currentTime,
