1c2b9b2a40734154f413425acd6db7a7809b0b46
Kilian Voss <kilianvoss@proton.me>
2023-12-27 14:20:41 +1100

    Button that loads config object into state and opens tthe configurator

---
+++ app/account/components/adminSavedConfigsResumeDraft.jsx
@@ -0,0 +1,27 @@
+"use client"
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import Link from "next/link";
+import { UseConfigurationReducerContext } from "@/app/context/globalContext.jsx";
+
+function ResumeDraftButton(props) {
+    const { state, dispatch } = UseConfigurationReducerContext();
+
+    const loadConfigurationIntoState = (configObject) => {
+        dispatch({ type: 'LOAD_CONFIGURATION_FROM_DB', payload: configObject })
+
+    }
+
+    return (
+        <Link href="/configurator">
+            <Button
+                variant="success"
+                onClick={() => { loadConfigurationIntoState(props.configFromDb) }}
+            >
+                Resume Configuration
+            </Button>
+        </Link>
+    );
+}
+
+export default ResumeDraftButton;
