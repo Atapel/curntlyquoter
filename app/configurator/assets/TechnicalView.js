b331ac5207616d836aa2a97e60a8d7cb029ba019
Kilian Voss <kilianvoss@proton.me>
2023-12-29 23:16:47 +1100

    SVG for third  view (on the right of the document)

---
+++ app/configurator/assets/TechnicalView.js
@@ -0,0 +1,8 @@
+export const technicalViewFrame = `<svg width="185" height="449" viewBox="0 0 185 449" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M89.3866 420.32V49.76M89.3866 420.32L82.9866 432.4M89.3866 420.32L96.0266 432.24M83.8666 46L83.1466 44.64L82.5066 43.12L81.1466 40.24L80.3466 38.96L79.4666 37.84L78.3466 37.04L77.0666 36.4L75.6266 36L74.1066 35.68L72.5066 35.52L70.9066 35.44L67.5466 35.28L65.9466 35.2L64.3466 34.96M82.6666 47.9679C82.6688 49.934 85.733 51.5244 89.5107 51.52C93.2883 51.5157 96.3488 49.9183 96.3466 47.9522C96.3443 45.986 93.2801 44.3957 89.5025 44.4C85.7248 44.4044 82.6643 46.0018 82.6666 47.9679ZM0.746643 448H184.187V0.559998H0.746643V448Z" stroke="black" stroke-width="0.56" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+<path d="M58.6666 432.4L65.0666 420.32L71.7066 432.24M64.9066 420.64L64.7466 400.48" stroke="black" stroke-width="0.96" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`
+
+export const technicalViewBreaker = `<svg width="61" height="8" viewBox="0 0 61 8" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M60.3866 7.44002H37.2666M0.626648 7.44002H11.9866M34.464 7.36552C32.6508 3.50297 28.7672 1.03755 24.5003 1.04001C20.2333 1.04249 16.3527 3.51235 14.5439 7.377" stroke="black" stroke-width="0.96" stroke-miterlimit="10" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>`
