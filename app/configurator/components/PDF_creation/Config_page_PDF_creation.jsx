4510cbb9ed190bbba6b3554214b178733337ac65
Kilian Voss <kilianvoss@proton.me>
2023-12-30 19:24:01 +1100

    First sketch of printing lines top right

--- app/configurator/components/PDF_creation/Config_page_PDF_creation.jsx
+++ app/configurator/components/PDF_creation/Config_page_PDF_creation.jsx
@@ -23,14 +23,34 @@ const PDF_Generation = ({ canvasRef }) => {
     });
     console.log("Current_User from PDF Creator", CurrentUser);
     pdf.addImage(canvasRef.current.toDataURL("image/png"), "PNG", 5, 5);
-    pdf.setFontSize(10);
+    
     // Set the text color, draw color, and font:
     pdf.setTextColor(0, 0, 0); // RGB values
     pdf.setDrawColor(255, 0, 0); // RGB values
     pdf.setFont("Helvetica", "normal"); // font name, style
     // Add some text to the document at a specific position:
+    pdf.setFontSize(7);
+    let linesTopRight = [
+      `CONSTRUCTION:PANELBOARD IS BUILT AND LABELED PER UL 891 `,
+      `EFFECTIVE AT TIME OF MANUFACTURE.`,
+      `SERVICE:INCOMING 1200A 120/208V 3-PHASE 4-WIRE.`,
+      `NEMA RATING:ENCLOSURE IS RATED NEMA 1 FOR INDOOR APPLICATIONS.`,
+      'FINISH:EXTERIOR FINISHED WITH ANSI 61 GRAY PAINT.',
+      `TERMINATIONS: TERMINATIONS ARE ACCESSIBLE FROM THE FRONT.`,
+      `BUSSING: SILVER PLATED COPPER SIZED IN ACCORDANCE WITH NYCEC AND NEC.`,
+      `NEUTRAL BUS IS FULL SIZE, RUN WITH THE PHASE`,
+      `BUSSES AND INSULATED FROM THE FRAME.`,
+      `BREAKERS: MCCB BY LS ELECTRIC.`,
+      `REFER TO ONE LINE DIAGRAM FOR MODEL NUMBERS`,]
 
-    var lines = [
+    var y = 50;
+
+    for (var i = 0; i < linesTopRight.length; i++) {
+      pdf.text(linesTopRight[i], 200, i * 5);
+      y += 5; // Increment y-coordinate for the new line
+    }
+    pdf.setFontSize(10);
+    var linesBottomRight = [
       `Project: ${state.Metadata.Project}`,
       `Client: ${state.Metadata.Client}`,
       `Equipment: ${0}`,
@@ -41,11 +61,11 @@ const PDF_Generation = ({ canvasRef }) => {
     ];
 
     // Start y-coordinate
-    var y = 170;
+    y = 170;
 
     // Print each line separately
-    for (var i = 0; i < lines.length; i++) {
-      pdf.text(lines[i], 240, y);
+    for (var i = 0; i < linesBottomRight.length; i++) {
+      pdf.text(linesBottomRight[i], 240, y);
       y += 5; // Increment y-coordinate for the new line
     }
 
