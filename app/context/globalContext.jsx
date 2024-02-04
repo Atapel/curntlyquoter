534c333c942e4e740fdb38e3313ab092908742af
Kilian Voss <kilianvoss@proton.me>
2024-02-04 23:44:03 +1100

    impleent new breaker reducer

--- app/context/globalContext.jsx
+++ app/context/globalContext.jsx
@@ -2,24 +2,30 @@
 import React, { createContext, useContext, useState, useReducer } from "react";
 // Import Logic
 import { initialConfiguration, reducer } from "./ConfigReducer"
+import { initialBreaker, breakerReducer } from "./BreakerReducer";
 // Create Context
 const Curent_User_Context = createContext();
 const ConfigurationReducerContext = createContext();
+const BreakerReducerContext = createContext();
 // Export the global Context Provider
 export const GlobalContextProvider = ({ children }) => {
   // Create the Context Variabless
   const [state, dispatch] = useReducer(reducer, initialConfiguration);
+  const [breakerState, breakerDispatch] = useReducer(breakerReducer, initialBreaker);  
   const [CurrentUser, setCurrentUser] = useState(false);
   return (
     <>
       <ConfigurationReducerContext.Provider
         value={{ state, dispatch }}
       >
-        <Curent_User_Context.Provider
-          value={{ CurrentUser, setCurrentUser }}
-        >
-          {children}
-        </Curent_User_Context.Provider>
+        <BreakerReducerContext.Provider
+          value={{ breakerState, breakerDispatch }}>
+          <Curent_User_Context.Provider
+            value={{ CurrentUser, setCurrentUser }}
+          >
+            {children}
+          </Curent_User_Context.Provider>
+        </BreakerReducerContext.Provider>
       </ConfigurationReducerContext.Provider>
     </>
   );
@@ -27,3 +33,4 @@ export const GlobalContextProvider = ({ children }) => {
 // Export the Context Consumers
 export const UseCurrentUserContext = () => useContext(Curent_User_Context);
 export const UseConfigurationReducerContext = () => useContext(ConfigurationReducerContext)
+export const UseBreakerReducerContext = () => useContext(BreakerReducerContext)
