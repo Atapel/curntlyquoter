b24dabf5322525aa6045f69579f6cb7c389f74c1
Kilian Voss <kilianvoss@proton.me>
2024-02-05 12:19:36 +1100

    rist iteration of displaying the price fetchen from google sheets, still buggy (executes fetch requests even when not prompted by user/button not clicked)

--- app/configurator/components/Quotation/Quotation_page_price.jsx
+++ app/configurator/components/Quotation/Quotation_page_price.jsx
@@ -1,34 +1,60 @@
 import React, { useEffect, useState } from 'react';
 import { UseConfigurationReducerContext } from "@/app/context/globalContext.jsx";
+import { Button } from 'react-bootstrap';
 
 const PriceDisplayComponent = () => {
   const { state, dispatch } = UseConfigurationReducerContext();
   const [data, setData] = useState('');
+  const [loading, setLoading] = useState(false);
+
+
+  const fetchData = async () => {
+    try {
+      setLoading(true);
+      const response = await fetch("http://localhost:3000/configurator/api_requests/google_sheet_call/pricing", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(state),
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      console.log(response);
+      const result = await response.json(); // Assuming the response is in JSON format
+      console.log(result);
+      setData(result);
+    } catch (error) {
+      console.error('Error fetching data:', error.message);
+    } finally {
+      setLoading(false);
+
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch('http://localhost:3000/configurator/api_requests/google_sheet_call'); // replace with your URL
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        const result = await response.text();
-        setData(result);
-      } catch (error) {
-        console.error('Error fetching data:', error.message);
-      }
-    };
-
     fetchData();
-
   }, []);
 
   return (
     <div>
-      <p>All details regarding the quotation will be listed here:</p>
-      <pre>{data}</pre>
+      <div>
+        <p>Pannel Price: ${Math.ceil(data.pannel)}</p>
+        <p>Breakers Price: ${Math.ceil(data.breakers)}</p>
+        <p>Total Price: ${Math.ceil(data.total)}</p>
+        <Button
+          variant="success"
+          onClick={fetchData}
+          disabled={loading}
+        >
+          {loading ? 'Loading...' : 'Generate Quote'}
+        </Button>
+      </div>
     </div>
   );
 };
 
 export default PriceDisplayComponent;
+
