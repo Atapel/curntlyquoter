e2bd317346508470d82b55336a26e2805ccf6606
Kilian Voss <kilianvoss@proton.me>
2024-02-05 14:36:22 +1100

    minro Changes to datastructure

--- app/context/BreakerReducer.js
+++ app/context/BreakerReducer.js
@@ -1,26 +1,36 @@
 export const initialBreaker = {
-    Selected_Size: "Select Breaker Size",
+    SelectedSize: "Select Breaker Size",
     SelectedTrip: "Select Breaker Trip",
     SelectedFeature: "Select Feature",
-    Selected_Breaker: { Description: "Select Breaker" },
+    SelectedBreaker: { Description: "Select Breaker" },
     SelectedBreakerAmp: "Select Amperage",
     SelectedBreakerPoles: "Select Poles",
 };
 
 export const breakerReducer = (state, action) => {
     switch (action.type) {
-        case SET_SELECTED_SIZE:
-            return { ...state, Selected_Size: action.payload };
-        case SET_SELECTED_TRIP:
+
+        case "RESET_BREAKER_STATE":
+            return initialBreaker;
+
+        case "SET_SELECTED_SIZE":
+            return { ...state, SelectedSize: action.payload };
+
+        case "SET_SELECTED_TRIP":
             return { ...state, SelectedTrip: action.payload };
-        case SET_SELECTED_FEATURE:
+
+        case "SET_SELECTED_FEATURE":
             return { ...state, SelectedFeature: action.payload };
-        case SET_SELECTED_BREAKER:
-            return { ...state, Selected_Breaker: action.payload };
-        case SET_SELECTED_BREAKER_AMP:
+
+        case "SET_SELECTED_BREAKER":
+            return { ...state, SelectedBreaker: action.payload };
+
+        case "SET_SELECTED_BREAKER_AMP":
             return { ...state, SelectedBreakerAmp: action.payload };
-        case SET_SELECTED_BREAKER_POLES:
+
+        case "SET_SELECTED_BREAKER_POLES":
             return { ...state, SelectedBreakerPoles: action.payload };
+
         default:
             return state;
     }
