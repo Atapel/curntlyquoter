8f9cff25bcbd5bf25ddf02ba0b6383a0eb3d2094
Kilian Voss <kilianvoss@proton.me>
2024-02-07 23:08:01 +1100

    using env

--- app/api_requests/google_sheet_call/pricing/modules/sheetsClient.js
+++ app/api_requests/google_sheet_call/pricing/modules/sheetsClient.js
@@ -1,6 +1,7 @@
 const { google } = require("googleapis");
 const getGoogleSheetsClient = () => {
-    const credentialsFilePath = "credentials.json"
+    // const credentialsFilePath = "credentials.json"
+    const credentialsFilePath = process.env.GOOGLE_SHEETS_CREDENTIALS_FILE_PATH
 
     const auth = new google.auth.GoogleAuth({
         keyFile: credentialsFilePath,
