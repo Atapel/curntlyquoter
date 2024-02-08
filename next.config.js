e18a0c6870d938e9015207470ec4433b8a8c310c
Kilian Voss <kilianvoss@proton.me>
2024-02-08 14:15:45 +1100

    added cors config template from the verrcel docs, lets see if it works

--- next.config.js
+++ next.config.js
@@ -1,4 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    reactStrictMode: true,
+    async headers() {
+      return [
+        {
+          source: '/api/:path*',
+          headers: [
+            {
+              key: 'Access-Control-Allow-Credentials',
+              value: 'true',
+            },
+            {
+              key: 'Access-Control-Allow-Origin', 
+              value: '*',
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+            },
+          ],
+        },
+      ]
+    }
+  }
+  
