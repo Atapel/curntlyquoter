59d889d60c192e98ab537df02048724a0218a498
Kilian Voss <kilianvoss@proton.me>
2024-02-10 17:50:13 +1100

    Revert "Changing it again"
    This reverts commit d4697401c0aa681863a1bfacc792f7cd1c9e8cc7.

--- next.config.js
+++ next.config.js
@@ -1,17 +1,27 @@
 module.exports = {
-  async headers() {
-    return [
-      {
-        // matching all API routes
-        source: "/api_requests/google_sheet_call/:path*",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
+    reactStrictMode: true,
+    async headers() {
+      return [
+        {
+        //   source: '/api_requests/:path*',
+          source: '/api_requests/(.*)',
+          headers: [
+            {
+              key: 'Access-Control-Allow-Credentials',
+              value: 'true',
+            },
+            {
+              key: 'Access-Control-Allow-Origin', 
+              value: '*',
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+            },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" }
+          ],
+        },
+      ]
+    }
   }
-};
-
+  
