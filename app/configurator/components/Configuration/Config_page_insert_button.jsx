fcc04fa7653a5270f8c5a5091b945e0dc7e0b08a
Kilian Voss <kilianvoss@proton.me>
2024-03-18 22:03:46 +0800

    logic ousourced in server acions

--- app/configurator/components/Configuration/Config_page_insert_button.jsx
+++ app/configurator/components/Configuration/Config_page_insert_button.jsx
@@ -1,94 +1,22 @@
 import { useState } from "react";
-import { createClient } from '../../../utils/supabase/clients'
 import { Button, Alert } from "react-bootstrap";
-import { UseCurrentUserContext, UseConfigurationReducerContext } from "../../../context/globalContext";
-
+import { UseConfigurationReducerContext } from "../../../context/globalContext";
+import { updateConfiguration }  from '../../../api_requests/supabase/actions'
 function SaveConfigurationButton() {
   const { state, dispatch } = UseConfigurationReducerContext();
-  const { CurrentUser, setCurrentUser } = UseCurrentUserContext();
   const [operationStatus, setOperationStatus] = useState(null);
 
-  const supabase = createClient();
-  const currentTime = new Date().toISOString().substring(0, 19).replace("T", " ");
-
   async function insertConfigurations() {
-    const objectToInsert = {
-      created_at: currentTime,
-      user_id: CurrentUser.User_UID,
-      init_client: state.Metadata.Client,
-      init_project: state.Metadata.Project,
-      init_drawn_by: CurrentUser.Given_Name + " " + CurrentUser.Family_Name,
-
-      panel_width: state.Configuration.SelectedFrameSize,
-      panel_voltage: state.Configuration.SelectedVoltage,
-      panel_KAIC_rating: state.Configuration.SelectedKAICRating,
-      panel_bus_rating: state.Configuration.SelectedBusRating,
-      selected_breakers: state.Configuration.SelectedBreakers,
-      order_confirmed: false,
+    
+    try {
+      updateConfiguration(state)
+      // Log success and set operation status
+      console.log("Record inserted successfully!");
+      setOperationStatus("success");
+    } catch (error) { 
+      console.log(error);
+      setOperationStatus("danger");
     }
-
-    if (state.Metadata.DatabaseID !== null) {
-      try {
-        console.log(state.Metadata.DatabaseID);
-        // Update entire row matching ID
-        const { data, updaterror } = await supabase
-          .from('Configurations')
-          .update(objectToInsert)
-          .eq('id', state.Metadata.DatabaseID)
-
-        // Handle data insertion error
-        if (updaterror) {
-          console.error("Supabase data update error:", updaterror.message, updaterror.details);
-          throw new Error("Failed to update record in the database.");
-        }
-
-        // Log success and set operation status
-        console.log("Record updated successfully!");
-        setOperationStatus("success");
-
-      } catch (error) {
-        // Overall error handling
-        console.error("Update configurations error:", error, error.message, error.details);
-        setOperationStatus("danger");
-      }
-
-    } else {
-
-      try {
-        // Try to insert data into the "Configurations" table
-        const { error: insertError } = await supabase.from("Configurations").insert({
-          created_at: currentTime,
-          user_id: CurrentUser.User_UID,
-          init_client: state.Metadata.Client,
-          init_project: state.Metadata.Project,
-          init_drawn_by: CurrentUser.Given_Name + " " + CurrentUser.Family_Name,
-
-          panel_width: state.Configuration.SelectedFrameSize,
-          panel_voltage: state.Configuration.SelectedVoltage,
-          panel_KAIC_rating: state.Configuration.SelectedKAICRating,
-          panel_bus_rating: state.Configuration.SelectedBusRating,
-          selected_breakers: state.Configuration.SelectedBreakers,
-          order_confirmed: false,
-        });
-
-        // Handle data insertion error
-        if (insertError) {
-          console.error("Supabase data insertion error:", insertError.message, insertError.details);
-          throw new Error("Failed to insert record into the database.");
-        }
-
-        // Log success and set operation status
-        console.log("Record inserted successfully!");
-        setOperationStatus("success");
-
-      } catch (error) {
-        // Overall error handling
-        console.error("Insert configurations error:", error, error.message, error.details);
-        setOperationStatus("danger");
-      }
-
-    }
-
   }
 
   return (
@@ -104,7 +32,7 @@ function SaveConfigurationButton() {
         <Alert variant={operationStatus}>
           {operationStatus === "success"
             ? "Configuration saved successfully!"
-            : "Failed to save configuration. Please try again or contact support."}
+            : "Failed to save configuration!"}
         </Alert>
       )}
     </>
