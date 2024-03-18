7aa70728d0f2fa0eaa041259b23a7f698fc1a46b
Kilian Voss <kilianvoss@proton.me>
2024-03-18 22:21:29 +0800

    shee now same name as db id so config and shee can be linked

--- app/api_requests/google_sheet_call/pricing/modules/cloneSheet.js
+++ app/api_requests/google_sheet_call/pricing/modules/cloneSheet.js
@@ -5,11 +5,6 @@ const clonePricingSheet = async (clienObject,configObject,spreadsheetId) => {
     //     SheetName: "Panel"
     //   }
     
-    //Untiil da6atabase id is implemented generate a dummy string and using it as sheet nameS 
-    const randomNumber = String(
-      Math.floor(Math.random() * (100000 - 1 + 1)) + 1
-    );
-
     try {
         // Step 1: Duplicate the existing sheet
         const duplicateSheetRequest = {
@@ -20,8 +15,7 @@ const clonePricingSheet = async (clienObject,configObject,spreadsheetId) => {
                         duplicateSheet: {
                             sourceSheetId: 344225992, // Provide the sheet name to be duplicated
                             insertSheetIndex: 1, // Choose the index where the new sheet will be inserted
-                            // newSheetName: configObject.Metadata.DatabaseID,
-                            newSheetName:randomNumber,
+                            newSheetName: configObject.Metadata.DatabaseID
                         },
                     },
                 ],
@@ -35,8 +29,7 @@ const clonePricingSheet = async (clienObject,configObject,spreadsheetId) => {
 
         // Return new sheet name, id
         return {
-            // sheetName: configObject.Metadata.DatabaseID,
-            sheetName: randomNumber,
+            sheetName: configObject.Metadata.DatabaseID,
             sheetId: newSheetId
         }
 
