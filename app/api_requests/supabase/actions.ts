3052d787a4381a9c056c251c5acf6cebfb7d456b
Kilian Voss <kilianvoss@proton.me>
2024-03-24 20:57:54 +0800

    confirm order logic now server sside

--- app/api_requests/supabase/actions.ts
+++ app/api_requests/supabase/actions.ts
@@ -77,26 +77,28 @@ export async function updateConfiguration(configState: any) {
       
 }
 
-// export async function confirmOrder() {
-//     const currentTime = new Date()
-//       .toISOString()
-//       .substring(0, 19)
-//       .replace("T", " ");
+export async function confirmOrder(configDbId: string) {
+    
+  const supabase = createClient()
+  
+    const currentTime = new Date()
+      .toISOString()
+      .substring(0, 19)
+      .replace("T", " ");
 
-//     try {
-//       console.log(CurrentUser);
-//       const { error } = await supabase.from("Configurations").update({
-//         order_confirmed: true,
-//       }).match({
-//         user_id: CurrentUser.id,
-//         created_at: currentTime,
-//       });
-//       if (error) {
-//         console.error("Supabase error:", error.message, error.details);
-//         throw new Error("Failed to confirm order.");
-//       }
-//       console.log("Order confirmed successfully!");
-//     } catch (error) {
-//       console.error(error);
-//     }
-//   }
+    try {
+      console.log();
+      const { error } = await supabase.from("Configurations").update({
+        order_confirmed: true,
+      }).match({
+        id: configDbId
+      });
+      if (error) {
+        console.error("Supabase error:", error.message, error.details);
+        throw new Error("Failed to confirm order.");
+      }
+      console.log("Order confirmed successfully!");
+    } catch (error) {
+      console.error(error);
+    }
+  }
