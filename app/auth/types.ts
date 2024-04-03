014670254ec31f6652139a45d802c3ccbeaaf6f9
Kilian Voss <kilianvoss@proton.me>
2024-04-03 14:54:06 +0700

    init

---
+++ app/auth/types.ts
@@ -0,0 +1,11 @@
+export interface ISignUpForm {
+    email: string;
+    password: string;
+    confirmPassword: string;
+  }
+export interface IConfirmSignupForm {
+    givenName: string,
+    familyName: string,
+    companyName: string,
+    phoneNumber: string
+}
