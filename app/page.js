5472ea37e4716d80687f5e8868ffe8722a38e13d
Kilian Voss <kilianvoss@proton.me>
2024-04-04 20:21:07 +0700

    delete

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 import Authenticate from "./auth/page"
-// import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import { createClient } from './utils/supabase/server'
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
