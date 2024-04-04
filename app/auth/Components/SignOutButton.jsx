55e2b65f8928a93247edc74d5c5029713a166293
Kilian Voss <kilianvoss@proton.me>
2024-04-04 19:38:26 +0700

    repaced the next redirect with the useEouter because of client side compatibility

--- app/auth/Components/SignOutButton.jsx
+++ app/auth/Components/SignOutButton.jsx
@@ -1,18 +1,19 @@
 'use client';
 import React from 'react';
-// import { createClient } from '../utils/supabase/server'
 import { createClient } from '../../utils/supabase/clients'
-import { redirect } from 'next/navigation'
+import { useRouter } from 'next/navigation';
 const SignOut = () => {
-    const supabase = createClient()
-    async function handleSignOut() {
-        const { error } = await supabase.auth.signOut();
-        if (error) {
-            // eslint-disable-next-line no-console
-            console.error('ERROR:', error);
-        }
-        redirect('/auth')
+    // Using Router instead of redirect here because of client side compitability
+  const router = useRouter();
+  const supabase = createClient()
+  async function handleSignOut() {
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.error('ERROR:', error);
+    } else {
+      router.push('/auth'); // Programmatic navigation after signout
     }
+  }
     return (
         <div className="container d-flex align-items-center justify-content-center mt-5">
             <button
