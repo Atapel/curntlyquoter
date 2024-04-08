942ea53105c2ef81a3ef22e594c54f610e7d2f34
Kilian Voss <kilianvoss@proton.me>
2024-04-08 23:55:36 +0700

    moved otp validation call into useEffect, still not perfect

--- app/auth/providers/emailPassword/confirmSignUp/querryParams.tsx
+++ app/auth/providers/emailPassword/confirmSignUp/querryParams.tsx
@@ -1,24 +1,31 @@
-"use clients"
+"use client"
+import { useEffect,useState } from 'react'
 import { validateOtp } from '../../../actions'
 import { useSearchParams } from 'next/navigation';
 export default function QuerryParams() {
-const searchParams = useSearchParams();
-    const tokenFromQueryParam = searchParams.get('token');
-    const emailFromQueryParam = searchParams.get('email');
-    console.log(tokenFromQueryParam,emailFromQueryParam);
-    // Handle potential missing parameters gracefully
-    if (!tokenFromQueryParam || !emailFromQueryParam) {
-        // Log an error or redirect to a different page
-        console.error('Missing confirmation parameters in URL.');
-        // You can redirect here using `useRouter` from `next/router`
-    }
-    //Make validateotp call
-    //Conditionall render error message in case otp couldnt be validated
-    //If no error then just redirect to confirmsignup page
+    const [error, setError] = useState(null);
+    const searchParams = useSearchParams();
+    const tokenFromQueryParam = searchParams.get('token')
+    const emailFromQueryParam = searchParams.get('email')
+    useEffect(() => {
+        const otpVal = async () => {
+            try{
+                await validateOtp( 
+                    emailFromQueryParam,
+                    tokenFromQueryParam 
+                )
+                console.log("Otp validated")
+            } catch (error){
+                setError(error.messages);
+                console.log("Error validating otp")
+            }
+        }
+        otpVal()
+    },[])
+
     return (
         <>
-            {String(tokenFromQueryParam)}
-            {String(emailFromQueryParam)}
+          {error && <p>Error: {error}</p>}
         </>
-    )
+      );
 }
