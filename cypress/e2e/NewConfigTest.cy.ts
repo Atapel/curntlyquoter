e928156323323bb7a9e4e8036bcd56690e95b935
Kilian Voss <kilianvoss@proton.me>
2024-04-22 00:01:18 +1000

    Commands inton seperate file

--- cypress/e2e/NewConfigTest.cy.ts
+++ cypress/e2e/NewConfigTest.cy.ts
@@ -1,3 +1,4 @@
+import "../support/commands"
 describe('Config Test Draft', () => {
   let authCookie;
 
@@ -6,65 +7,33 @@ describe('Config Test Draft', () => {
     cy.get('[data-testid="email-input"]').type('kilian96@live.de');
     cy.get('[data-testid="password-input"]').type('Kilian96');
     cy.get('[data-testid="signin-button"]').click();
-    // cy.getCookie('sb-wnlbohioemzhzlhhknvy-auth-token').then(cookie => {
-    //   authCookie = cookie;
-    // })
-    cy.wait(1000)
-
-    const random = Math.floor(Math.random() * 1000);
-    cy.get('[data-testid="project-input"]').type(`Test${random}`);
-    cy.get('[data-testid="client-input"]').type(`Test${random}`);
-    cy.get('[data-testid="launch-new-configuration-button"]').click();
-
-    cy.wait(1000)
-
-    cy.get(`[data-testid="Dropdown-Width"]`).click();
-    cy.get(`[data-testid="selection-36"]`).click()
-
-    cy.get(`[data-testid="Dropdown-Height"]`).click();
-    cy.get(`[data-testid="selection-90"]`).click()
-
-    cy.get(`[data-testid="Dropdown-Voltage"]`).click();
-    cy.get(`[data-testid="selection-120V"]`).click()
-
-    cy.get(`[data-testid="Dropdown-Kaic"]`).click();
-    cy.get(`[data-testid="selection-100"]`).click()
-
-    cy.get(`[data-testid="Dropdown-Bus"]`).click();
-    cy.get(`[data-testid="selection-1500"]`).click()
     
-    cy.get(`[data-testid="Dropdown-DistService"]`).click();
-    cy.get(`[data-testid="selection-Distribution"]`).click()
+    cy.getCookie('sb-wnlbohioemzhzlhhknvy-auth-token').then(cookie => {
+      authCookie = cookie;
+    })
 
-    cy.get(`[data-testid="Dropdown-Feed"]`).click();
-    cy.get(`[data-testid="selection-MainBreaker"]`).click()
+    cy.wait(5000)
 
-    cy.get(`[data-testid="Add-Frame"]`).click();
+    cy.launchNewConfig()
+    
+    cy.wait(5000)
 
-    cy.get(`[data-testid="Dropdown-BreakerFrame"]`).click();
-    cy.get(`[data-testid="selection-Single"]`).click()
+    cy.addFrame(
+      36,
+      90,
+      "120V",
+      "100",
+      "1500",
+      "Distribution",
+      "MainBreaker",
+    )
+    cy.addBreaker(
+      "Single",
+      "UTS400",
+      "250",
+      "2p"
+    )
 
-    cy.get(`[data-testid="Dropdown-Breaker"]`).click();
-    cy.get(`[data-testid="selection-UTS400"]`).click()
-
-    cy.get(`[data-testid="Dropdown-Amp"]`).click();
-    cy.get(`[data-testid="selection-250"]`).click()
-
-    cy.get(`[data-testid="Dropdown-Poles"]`).click();
-    cy.get(`[data-testid="selection-2p"]`).click()
-
-    cy.get(`[data-testid="Add-Breaker"]`).click();
   })
 
-  // it("Launches Configurator", () => {
-  //   cy.visit('http://localhost:3000/account')
-  //   const random = Math.floor(Math.random() * 1000);
-  //   cy.get('[data-testid="project-input"]').type(`Test${random}`);
-  //   cy.get('[data-testid="client-input"]').type(`Test${random}`);
-  //   cy.get('[data-testid="launch-new-configuration-button"]').click();
-  // })
-
 })
-
-// Save Auth Token
-// Make Each Possible frame combionation its own test
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,37 +1,87 @@
 /// <reference types="cypress" />
-// ***********************************************
-// This example commands.ts shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-//
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+    interface Chainable<Subject = any> {
+      addFrame(
+        width: number,
+        height: number,
+        voltage: string,
+        kaicRating: string,
+        busRating: string,
+        distributionService: string,
+        feedType: string
+      ): Chainable<any>;
+    }
+    interface Chainable<Subject = any> {
+        addBreaker(
+            breakerFrame: string, 
+            breaker: string, 
+            amp: string, 
+            poles: string
+        ): Chainable<any>;
+      }
+      interface Chainable<Subject = any> {
+        launchNewConfig(): Chainable<any>;
+      }
+  }
+Cypress.Commands.add(`launchNewConfig`, ()=>{
+    cy.visit(`/`);
+    const random = Math.floor(Math.random() * 1000);
+    cy.get('[data-testid="project-input"]').type(`Test${random}`);
+    cy.get('[data-testid="client-input"]').type(`Test${random}`);
+    cy.get('[data-testid="launch-new-configuration-button"]').click();
+
+})
+Cypress.Commands.add(`addFrame`, (
+    width,
+    height,
+    voltage,
+    kaicRating,
+    busRating,
+    distributionService,
+    feedType
+    ) => {
+    cy.get(`[data-testid="Dropdown-Width"]`).click();
+    cy.get(`[data-testid="selection-${width}"]`).click()
+
+    cy.get(`[data-testid="Dropdown-Height"]`).click();
+    cy.get(`[data-testid="selection-${height}"]`).click()
+
+    cy.get(`[data-testid="Dropdown-Voltage"]`).click();
+    cy.get(`[data-testid="selection-${voltage}"]`).click()
+
+    cy.get(`[data-testid="Dropdown-Kaic"]`).click();
+    cy.get(`[data-testid="selection-${kaicRating}"]`).click()
+
+    cy.get(`[data-testid="Dropdown-Bus"]`).click();
+    cy.get(`[data-testid="selection-${busRating}"]`).click()
+    
+    cy.get(`[data-testid="Dropdown-DistService"]`).click();
+    cy.get(`[data-testid="selection-${distributionService}"]`).click()
+
+    cy.get(`[data-testid="Dropdown-Feed"]`).click();
+    cy.get(`[data-testid="selection-${feedType}"]`).click()
+
+    cy.get(`[data-testid="Add-Frame"]`).click();
+  });
+
+
+Cypress.Commands.add('addBreaker', (breakerFrame, breaker, amp, poles) => {
+    cy.get(`[data-testid="Dropdown-BreakerFrame"]`).click();
+    cy.get(`[data-testid="selection-${breakerFrame}"]`).click();
+  
+    cy.get(`[data-testid="Dropdown-Breaker"]`).click();
+    cy.get(`[data-testid="selection-${breaker}"]`).click();
+  
+    cy.get(`[data-testid="Dropdown-Amp"]`).click();
+    cy.get(`[data-testid="selection-${amp}"]`).click();
+  
+    cy.get(`[data-testid="Dropdown-Poles"]`).click();
+    cy.get(`[data-testid="selection-${poles}"]`).click();
+
+    cy.get(`[data-testid="Add-Breaker"]`).click();
+  });
+
+  
+  
