cd06704004acd2f4f28f10133b7fd7d035c5c6bf
Kilian Voss <kilianvoss@proton.me>
2024-04-28 20:46:12 +1000

    Removed unused props and added conditional && renders (no breaking changes)

--- app/configurator/components/Configuration/Config_page_Selected_Breakers_List.jsx
+++ app/configurator/components/Configuration/Config_page_Selected_Breakers_List.jsx
@@ -3,22 +3,17 @@ import ListGroup from "react-bootstrap/ListGroup";
 import { Alert, Button, Col, Dropdown, Row } from "react-bootstrap";
 import { UseConfigurationReducerContext } from "@/app/context/globalContext.jsx";
 
-const DisplaySelectedItems = (props) => {
+const DisplaySelectedItems = () => {
   const { state, dispatch } = UseConfigurationReducerContext();
-  // const [renderSelectedBrakers, setrenderSelectedBrakers] = props.renderstate
 
   const deleteItem = (indexToDelete) => {
-    // if(state.Configuration.SelectedBreakers.length === 1){
-    //   setrenderSelectedBrakers(false)
-    //   // console.log("renderSelectedBrakers",renderSelectedBrakers);
-    // }
     dispatch({ type: 'REMOVE_BREAKER', payload: indexToDelete })
   };
   return (
     <div>
       <ListGroup>
 
-        { state.Configuration.SelectedBreakers.length === 0 ? (
+        {state.Configuration.SelectedBreakers && state.Configuration.SelectedBreakers.length === 0 ? (
           // <Alert variant="info">
           //   Please select some breakers
           // </Alert>
@@ -30,7 +25,7 @@ const DisplaySelectedItems = (props) => {
           </ListGroup.Item>
         )}
 
-        {state.Configuration.SelectedBreakers.map((item, index) => (
+        {state.Configuration.SelectedBreakers && state.Configuration.SelectedBreakers.map((item, index) => (
           <ListGroup.Item key={index}>
             <Row>
               <Col>{item.Name}</Col>
