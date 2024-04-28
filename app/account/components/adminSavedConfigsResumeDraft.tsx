6db7731dcd3e65e6152265ee52bcfdb19646a834
Kilian Voss <kilianvoss@proton.me>
2024-04-28 20:54:37 +1000

    adding props type checking

--- app/account/components/adminSavedConfigsResumeDraft.tsx
+++ app/account/components/adminSavedConfigsResumeDraft.tsx
@@ -1,19 +1,20 @@
 "use client"
-import React from 'react';
+import {FunctionComponent} from 'react';
 import { Button } from 'react-bootstrap';
 import Link from "next/link";
 import { UseConfigurationReducerContext } from "../../context/globalContext";
 import { TConfigDB } from "../../context/types";
-function ResumeDraftButton(props) {
+interface IResumeDraftButtonProps {
+    configFromDb: TConfigDB
+}
+function ResumeDraftButton(props:IResumeDraftButtonProps):FunctionComponent<IResumeDraftButtonProps>{
     const { state, dispatch } = UseConfigurationReducerContext();
-
     const loadConfigurationIntoState = (configObject: TConfigDB) => {
         dispatch({ 
             type: 'LOAD_CONFIGURATION_FROM_DB', 
             payload: configObject 
         })
     }
-
     return (
         <Link href="/configurator">
             <Button
@@ -25,5 +26,4 @@ function ResumeDraftButton(props) {
         </Link>
     );
 }
-
 export default ResumeDraftButton;
