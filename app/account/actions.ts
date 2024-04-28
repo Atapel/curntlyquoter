e37421aa3eb8ec3722661aa285e1ddb9c7c8d1eb
Kilian Voss <kilianvoss@proton.me>
2024-04-28 20:34:55 +1000

    outsourcing deletion and fetching of svaed coinfigurations from the DB

---
+++ app/account/actions.ts
@@ -0,0 +1,48 @@
+"use server";
+import { createClient } from '../utils/supabase/server'
+import { deletePricingSubSheet }  from '../api_requests/google_sheet_call/pricing/deleteSubSheet/actions'
+import { TConfigDB } from '../context/types';
+import { revalidatePath } from 'next/cache'
+const supabase = createClient();
+const urlPath = process.env.NEXT_PUBLIC_PRICING_SHEET_ROUTEHANDLER_URL;
+export async function getConfigs() {
+    const {
+        data: { session },
+      } = await supabase.auth.getSession();
+    const userId = session?.user.id;
+  try {
+    const { data , error } = await supabase
+      .from("Configurations")
+      .select()
+      .eq("user_id", userId);
+    if (error) {
+      throw new Error("Failed to retrieve data from the database.");
+    }
+    return data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+export async function deleteConfigs(id: string) {
+  try {
+    try {
+      await deletePricingSubSheet(id)
+    } catch (error) {
+      console.error(error);
+    }
+    
+    // Delete Configurations from DB
+    const { error } = await supabase
+      .from("Configurations")
+      .delete()
+      .eq("id", id);
+      revalidatePath('/account')
+    if (error) {
+      throw new Error("Failed to delete data from the database.");
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
