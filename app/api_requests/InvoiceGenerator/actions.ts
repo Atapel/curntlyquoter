e02cfae80040800b8bbdab55257ed207de7d6252
Kilian Voss <kilianvoss@proton.me>
2024-05-19 20:45:50 +1000

    init

---
+++ app/api_requests/InvoiceGenerator/actions.ts
@@ -0,0 +1,11 @@
+"use server"
+
+// Get TConfigState
+
+// Either fetch Odoo from here or
+
+// Call google sheet call api
+// Possibly scrape ls electric for breaker specified pricing info
+// Create Document
+// Either send document in some svg or other raw format
+// Or send a pdf with jspdf library
---
+++ app/api_requests/LS_electric/actions.ts
@@ -0,0 +1,9 @@
+"use server"
+// Get TConfigState
+// Fetch Api
+// Get all available breakers 
+// and match with the breakers that are currently in TConfigState
+// For each selected breaker add availability infos
+
+// Trigger this function from within a config reducer method that then updates the state 
+// with all the new availabitlity infos 
