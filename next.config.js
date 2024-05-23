3be7b25446667d5e313c5bb448fd8c4d2b15f4b1
Kilian Voss <kilianvoss@proton.me>
2024-05-23 17:26:04 +1000

    remove ts error surpression config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 module.exports = {
     reactStrictMode: true,
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-    },
     async headers() {
       return [
         {
