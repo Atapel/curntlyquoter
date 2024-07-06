5c12cd6185eb39380414e7a21c6ed41aea6f4d82
Kilian Voss <kilianvoss@proton.me>
2024-07-06 14:54:26 +0800

    Using next Router to redirect to configurator. Solved bug that prevented configs from saving. In previous version the button acted as href/link, so when pressed it immediately redirected to ./configuraor without running and finishing the handle submit function propperly. Because of that the Database Id could not be set at dispatch call INIT NEW CONFIG. JSX still needs to be rewritten to using normal bootstrap classes.

--- app/account/components/adminUserInput.tsx
+++ app/account/components/adminUserInput.tsx
@@ -1,10 +1,10 @@
 "use client";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { Form, Button, Row, Col, Container, ListGroup } from "react-bootstrap";
 import { UseConfigurationReducerContext } from "@context/globalContext";
-import {insertConfigurationInit}  from '@api_requests/supabase/actions'
-
+import { insertConfigurationInit } from "@api_requests/supabase/actions";
+import { useRouter } from "next/navigation";
 const NewConfigInput = () => {
   const { state, dispatch } = UseConfigurationReducerContext();
   const [formData, setFormData] = useState({
@@ -12,6 +12,7 @@ const NewConfigInput = () => {
     project: "",
   });
   const [isFormIncomplete, setIsFormIncomplete] = useState(true);
+  const router = useRouter();
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -27,28 +28,31 @@ const NewConfigInput = () => {
       alert("Please fill out all fields");
       return;
     }
-    
+
     // add error handling
     const Id = await insertConfigurationInit({
       Client: formData.client,
-      Project: formData.project
-    })
-    
-    dispatch({ type: 'TOTAL_RESET' })
+      Project: formData.project,
+    });
+
+    dispatch({ type: "TOTAL_RESET" });
     // Typescript didn notice the wrong payload i gave it
     // Payload arg was payload: {
-      // client: formData.client,
-      // project: formData.project,
-      // databaseId: Id
-      // Capitalisation in key was wrong, no error recieved
-    dispatch({ type: 'INIT_NEW_CONFIG', payload: {
-      Client: formData.client,
-      Project: formData.project,
-      DatabaseID: Id,
-      ResumeDraft: false
-    }})
-    console.log("state after init",state);
-    
+    // client: formData.client,
+    // project: formData.project,
+    // databaseId: Id
+    // Capitalisation in key was wrong, no error recieved
+    dispatch({
+      type: "INIT_NEW_CONFIG",
+      payload: {
+        Client: formData.client,
+        Project: formData.project,
+        DatabaseID: Id,
+        ResumeDraft: false,
+      },
+    });
+    console.log("state after init", state);
+    router.push("/configurator"); // Programmatic navigation after signout
   };
 
   return (
@@ -98,18 +102,17 @@ const NewConfigInput = () => {
             </Container>
 
             {/* TODO: Grey out button when no input in both fields */}
-            <Link href="/configurator">
-              <Button
-                variant="outline-success"
-                className="w-100"
-                onClick={handleSubmit} // Use onClick to trigger the function
-                disabled={isFormIncomplete}
-                data-testid="launch-new-configuration-button"
-              >
-                Launch Configurator
-              </Button>
-            </Link>
-
+            {/* <Link href="/configurator"> */}
+            <Button
+              variant="outline-success"
+              className="w-100"
+              onClick={handleSubmit} // Use onClick to trigger the function
+              disabled={isFormIncomplete}
+              data-testid="launch-new-configuration-button"
+            >
+              Launch Configurator
+            </Button>
+            {/* </Link> */}
           </Form>
         </ListGroup.Item>
       </ListGroup>
@@ -118,3 +121,39 @@ const NewConfigInput = () => {
 };
 
 export default NewConfigInput;
+
+{/* <>
+<div className="border" style={{ margin: "1rem" }}>
+  <h2>New configuration</h2>
+  <form className="border p-2 form-group d-flex flex-row justify-between">
+    <label htmlFor="formClient">Client</label>
+    <input
+      type="text"
+      className="form-control mx-3"
+      id="formClient"
+      name="client"
+      value={formData.client}
+      onChange={handleChange}
+      data-testid="client-input"
+    />
+    <label htmlFor="formProject">Project</label>
+    <input
+      type="text"
+      className="form-control mx-3"
+      id="formProject"
+      name="project"
+      value={formData.project}
+      onChange={handleChange}
+      data-testid="project-input"
+    />
+    <button
+      className="btn btn-outline-success w-50"
+      onClick={handleSubmit}
+      // disabled={isFormIncomplete}
+      data-testid="launch-new-configuration-button"
+    >
+      Launch Configurator
+    </button>
+  </form>
+</div>
+</> */}
