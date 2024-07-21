0dcb1127a8a2b39c7e9886fee355e4385f5cf192
Kilian Voss <kilianvoss@proton.me>
2024-07-21 13:41:36 +0200

    Fixed object key that prevented rendering the feedthru lugs svg.

--- app/configurator/components/PDF_creation/SvgSourceGenerator.tsx
+++ app/configurator/components/PDF_creation/SvgSourceGenerator.tsx
@@ -1,3 +1,4 @@
+import {TConfiguration} from "@context/types"
 import {
   container_46_w_dimensions_SVG,
   container_36_w_dimensions_SVG,
@@ -17,7 +18,7 @@ export function frontViewSvgSource(configStateObject) {
     return ("data:image/svg+xml," + encodeURIComponent(container_46_w_dimensions_SVG))
   }   
 }
-export function sideViewSvgSource(configStateObject) {
+export function sideViewSvgSource(configStateObject: TConfiguration) {
   let sideViewSource = sideViewFrame
 
   if (configStateObject.SelectedFeedType === "Main Lug") {
@@ -30,7 +31,7 @@ export function sideViewSvgSource(configStateObject) {
       console.log('Bottom');
       sideViewSource = sideViewSource + encodeURIComponent(sideViewMainLugBottom);
     }
-    else if(configStateObject.SelectedFeedPosition === "FeedThru"){
+    else if(configStateObject.SelectedFeedPosition === "FeedThru (Top/Bottom)"){
       console.log('FeedThru');
       sideViewSource = sideViewSource + encodeURIComponent(sideViewMainLugTop + sideViewMainLugBottom);
     }
